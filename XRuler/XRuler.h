51e1a8d1b6156fe0c1bd70d09ee3eacfbee0f8ed
networkextension <b@gmail.com>
2017-11-23 13:59:38 +0800

    Initial Commit

---
+++ XRuler/XRuler.h
@@ -0,0 +1,19 @@
+//
+//  XRuler.h
+//  XRuler
+//
+//  Created by yarshure on 2017/11/23.
+//  Copyright © 2017年 yarshure. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for XRuler.
+FOUNDATION_EXPORT double XRulerVersionNumber;
+
+//! Project version string for XRuler.
+FOUNDATION_EXPORT const unsigned char XRulerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XRuler/PublicHeader.h>
+
+
