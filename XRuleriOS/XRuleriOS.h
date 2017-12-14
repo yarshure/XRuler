cc67b5c76ad995c9b96fb3908d13547bee7b85da
yarshure <support@abigt.net>
2017-12-14 15:07:11 +0800

    add iOS target

---
+++ XRuleriOS/XRuleriOS.h
@@ -0,0 +1,19 @@
+//
+//  XRuleriOS.h
+//  XRuleriOS
+//
+//  Created by yarshure on 2017/12/14.
+//  Copyright © 2017年 yarshure. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XRuleriOS.
+FOUNDATION_EXPORT double XRuleriOSVersionNumber;
+
+//! Project version string for XRuleriOS.
+FOUNDATION_EXPORT const unsigned char XRuleriOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XRuleriOS/PublicHeader.h>
+
+
