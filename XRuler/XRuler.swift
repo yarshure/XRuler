b8294f5d06502cfcc5e4efae248f9036f733a3e1
yarshure <yarshure@gmail.com>
2018-01-08 14:53:16 +0800

    update os_log

--- XRuler/XRuler.swift
+++ XRuler/XRuler.swift
@@ -10,18 +10,28 @@ import Foundation
 import AxLogger
 import os.log
 public class XRuler {
+    static var debugEnable = false
     static func log(_ msg:String,level:AxLoggerLevel , category:String="default",file:String=#file,line:Int=#line,ud:[String:String]=[:],tags:[String]=[],time:Date=Date()){
         
         if level != AxLoggerLevel.Debug {
             AxLogger.log(msg,level:level)
         }
+        #if os(iOS)
+            if #available(iOSApplicationExtension 10.0, *) {
+                os_log("XRuler: %@", log: .default, type: .debug, msg)
+            } else {
+                print(msg)
+                
+            }
+        #elseif os(macOS)
+            if #available(iOSApplicationExtension 10.12, *) {
+                os_log("XRuler: %@", log: .default, type: .debug, msg)
+            } else {
+                print(msg)
+                
+            }
+        #endif
         
-        if #available(OSXApplicationExtension 10.12, *) {
-            os_log("XRuler: %@", log: .default, type: .debug, msg)
-        } else {
-            print(msg)
-            // Fallback on earlier versions
-        }
     }
     public static var kProxyGroupFile:String = ".ProxyGroup"
     public static var groupIdentifier:String = ""
@@ -31,12 +41,24 @@ public class XRuler {
         if level != AxLoggerLevel.Debug {
             AxLogger.log(msg,level:level)
         }
-        if #available(OSXApplicationExtension 10.12, *) {
-            os_log("XRuler: %@", log: .default, type: .debug, msg)
-        } else {
-            print(msg)
-            // Fallback on earlier versions
+        if debugEnable {
+            #if os(iOS)
+                if #available(iOSApplicationExtension 10.0, *) {
+                    os_log("XRuler: %@", log: .default, type: .debug, msg)
+                } else {
+                    print(msg)
+                    
+                }
+            #elseif os(macOS)
+                if #available(iOSApplicationExtension 10.12, *) {
+                    os_log("XRuler: %@", log: .default, type: .debug, msg)
+                } else {
+                    print(msg)
+                    
+                }
+            #endif
         }
+        
     }
     public static func load(_ group:String,proxy:String){
         XRuler.groupIdentifier = group
