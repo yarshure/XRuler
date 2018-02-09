7364f7d709c3192e7c4e394ca3bb752d49f1ada4
yarshure <yarshure@gmail.com>
2018-02-09 23:12:39 +0800

    automate build debug

---
+++ codesign.sh
@@ -0,0 +1,11 @@
+#carthage update   --cache-builds --platform mac --configuration Debug
+export CODESIGN_ALLOCATE=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/codesign_allocate
+    
+cd Carthage/Build/Mac/
+
+
+find . -name '*.framework' -type d | while read -r FRAMEWORK
+do
+echo "codesign $FRAMEWORK"
+/usr/bin/codesign --sign  16B002250F14633E0CC0B2915EB8587D50772A42 --force --preserve-metadata=identifier,entitlements,flags --timestamp=none "$FRAMEWORK/Versions/A"
+done
